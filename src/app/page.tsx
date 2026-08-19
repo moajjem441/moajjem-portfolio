d61ac027fceb5a05a90994df8dd006e89c584059
Moajjem Hossain <moajjem441@gmail.com>
2026-08-20 00:53:14 +0600

    skillSpere updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1055,7 +1055,7 @@ export default function Home() {
                     "HeroUI",
                     "DaisyUI"
                   ],
-                  href: "/projects/skillsphere",     // ✅ নতুন
+                  href: "/projects/skillSphere",     // ✅ নতুন
                   github: "https://github.com/moajjem441/Skill-Sphere",
                   live: "https://skill-sphere-taupe.vercel.app"
                 },
